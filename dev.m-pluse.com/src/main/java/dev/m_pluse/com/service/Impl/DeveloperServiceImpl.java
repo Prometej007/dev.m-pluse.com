ba202e4f70b24909b6f0b22b155cf4caadc276a4
danul <danul@DESKTOP-0MKJDAG>
2017-04-02 22:17:00 +0300

    add fix service

--- dev.m-pluse.com/src/main/java/dev/m_pluse/com/service/Impl/DeveloperServiceImpl.java
+++ dev.m-pluse.com/src/main/java/dev/m_pluse/com/service/Impl/DeveloperServiceImpl.java
@@ -1,18 +1,23 @@
 package dev.m_pluse.com.service.Impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.m_pluse.com.dao.DeveloperDao;
+import dev.m_pluse.com.entity.Department;
 import dev.m_pluse.com.entity.Developer;
+import dev.m_pluse.com.entity.Session;
 import dev.m_pluse.com.service.DeveloperService;
+
 @Service
 public class DeveloperServiceImpl implements DeveloperService {
 
 	@Autowired
 	private DeveloperDao developerDao;
+	
 
 	public void save(Developer developer) {
 		developerDao.save(developer);
@@ -31,5 +36,22 @@ public class DeveloperServiceImpl implements DeveloperService {
 		developerDao.delete(id);
 
 	}
+
+	/**
+	 * 
+	 * @param department
+	 * @return повертає всіх працівників відділу department
+	 */
+	public List<Developer> allDeveloperOfTheDepartment(Department department) {
+		List<Developer> list = new ArrayList<Developer>();
+		for (Developer developer : developerDao.findAll()) {
+			if (developer.getDepartment().equals(department)) {
+				list.add(developer);
+			}
+		}
+		return list;
+	}
+
+	
 
 }
--- dev.m-pluse.com/src/main/java/dev/m_pluse/com/service/Impl/SessionServiceImpl.java
+++ dev.m-pluse.com/src/main/java/dev/m_pluse/com/service/Impl/SessionServiceImpl.java
@@ -1,10 +1,14 @@
 package dev.m_pluse.com.service.Impl;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import dev.m_pluse.com.dao.SessionDao;
+import dev.m_pluse.com.entity.Department;
+import dev.m_pluse.com.entity.Developer;
 import dev.m_pluse.com.entity.Session;
 import dev.m_pluse.com.service.SessionService;
 
@@ -32,4 +36,222 @@ public class SessionServiceImpl implements SessionService {
 		sessionDao.delete(id);
 	}
 
+	/**
+	 * 
+	 * @return робочу сесію розробників за поточний місяць
+	 */
+	public List<Session> sessionPerMonth() {
+		LocalDateTime date = LocalDateTime.now();
+		List<Session> list = new ArrayList();
+		for (Session session : sessionDao.findAll()) {
+			if (date.getMonth().equals(session.getFinishSession().getMonth())) {
+				list.add(session);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param developer
+	 * @return робочу сесію розробника за поточний місяць
+	 */
+
+	public List<Session> sessionPerMonth(Developer developer) {
+		LocalDateTime date = LocalDateTime.now();
+		List<Session> list = new ArrayList();
+		for (Session session : sessionDao.findAll()) {
+			if (date.getMonth().equals(session.getFinishSession().getMonth())
+					&& developer.equals(session.getDeveloper())) {
+				list.add(session);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param department
+	 * @return робочу сесію розробників з department за поточний місяць
+	 */
+	public List<Session> sessionPerMonth(Department department) {
+		LocalDateTime date = LocalDateTime.now();
+		List<Session> list = new ArrayList();
+		for (Session session : sessionDao.findAll()) {
+			if (date.getMonth().equals(session.getFinishSession().getMonth())
+					&& department.equals(session.getDeveloper().getDepartment())) {
+				list.add(session);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param numberMonth
+	 * @param numbeYear
+	 * @return - list сесій розробників за numbeYear і numberMonth
+	 */
+	public List<Session> sessionPerMonth(int numberMonth, int numbeYear) {
+
+		List<Session> list = new ArrayList();
+		for (Session session : sessionDao.findAll()) {
+			if (session.getStartSession().getMonthValue() == numberMonth
+					&& session.getStartSession().getYear() == numbeYear) {
+				list.add(session);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param numberMonth
+	 * @param numbeYear
+	 * @param department
+	 * @return - list сесій розробників за numbeYear і numberMonth і з відділу
+	 */
+	public List<Session> sessionPerMonth(int numberMonth, int numbeYear, Department department) {
+
+		List<Session> list = new ArrayList();
+		for (Session session : sessionDao.findAll()) {
+			if (session.getStartSession().getMonthValue() == numberMonth
+					&& session.getStartSession().getYear() == numbeYear
+					&& department.equals(session.getDeveloper().getDepartment())) {
+				list.add(session);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param numberMonth
+	 * @param numberYear
+	 * @param developer
+	 * @return - list сесій розробника developer за numbeYear і numberMonth
+	 */
+	public List<Session> sessionPerMonth(int numberMonth, int numberYear, Developer developer) {
+
+		List<Session> list = new ArrayList();
+		for (Session session : sessionDao.findAll()) {
+			if (session.getStartSession().getMonthValue() == numberMonth
+					&& session.getStartSession().getYear() == numberYear && developer.equals(session.getDeveloper())) {
+				list.add(session);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param numberYear
+	 * @return - list сесій розробника за numbeYear
+	 */
+	public List<Session> sessionPerYear(int numberYear) {
+
+		List<Session> list = new ArrayList();
+		for (Session session : sessionDao.findAll()) {
+			if (session.getStartSession().getYear() == numberYear) {
+				list.add(session);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param numberYear
+	 * @param department
+	 * @return - list сесій розробника за numbeYear з department
+	 */
+	public List<Session> sessionPerYear(int numberYear, Department department) {
+
+		List<Session> list = new ArrayList();
+		for (Session session : sessionDao.findAll()) {
+			if (session.getStartSession().getYear() == numberYear
+					&& department.equals(session.getDeveloper().getDepartment())) {
+				list.add(session);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param numberYear
+	 * @param developer
+	 * @return - list сесій розробника developer за numbeYear
+	 */
+	public List<Session> sessionPerYear(int numberYear, Developer developer) {
+
+		List<Session> list = new ArrayList();
+		for (Session session : sessionDao.findAll()) {
+			if (session.getStartSession().getYear() == numberYear && developer.equals(session.getDeveloper())) {
+				list.add(session);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param developer
+	 *            - відкриття деної сесії розробника
+	 */
+	public void createNewDaySession(Developer developer) {
+		Session session = new Session();
+		session.setDeveloper(developer);
+		session.setStartSession(LocalDateTime.now());
+		save(session);
+	}
+
+	/**
+	 * 
+	 * @param developer
+	 * @param report
+	 *            звіт String закінчення робочої сесії з коментарем string
+	 */
+	public void finishDaySessionReport(Developer developer, String report) {
+		Session saveSession = null;
+		for (Session session : sessionDao.findAll()) {
+			if (session.getStartSession().getYear() == LocalDateTime.now().getYear()
+					&& session.getStartSession().getMonth().equals(LocalDateTime.now().getMonth())
+					&& session.getStartSession().getDayOfMonth() == LocalDateTime.now().getDayOfMonth()
+					&& developer.equals(session.getDeveloper()) && session.getFinishSession() == null) {
+				saveSession = session;
+			}
+		}
+		if (saveSession != null && !report.equals("")) {
+			saveSession.setFinishSession(LocalDateTime.now());
+			saveSession.setReport(report);
+			save(saveSession);
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param developer
+	 * @param path
+	 *            - посилання на звіт закінчення робочої сесії з звітом в файлі
+	 */
+	public void finishDaySessionPath(Developer developer, String path) {
+		Session saveSession = null;
+		for (Session session : sessionDao.findAll()) {
+			if (session.getStartSession().getYear() == LocalDateTime.now().getYear()
+					&& session.getStartSession().getMonth().equals(LocalDateTime.now().getMonth())
+					&& session.getStartSession().getDayOfMonth() == LocalDateTime.now().getDayOfMonth()
+					&& developer.equals(session.getDeveloper()) && session.getFinishSession() == null) {
+				saveSession = session;
+			}
+		}
+		if (saveSession != null && !path.equals("")) {
+			saveSession.setFinishSession(LocalDateTime.now());
+			saveSession.setPathReport(path);
+			save(saveSession);
+			;
+		}
+
+	}
 }
