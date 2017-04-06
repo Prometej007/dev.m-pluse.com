36830e493366feb26208a2a3e3689d5277476942
Anblak <anazariks@gmail.com>
2017-04-06 12:24:44 +0300

    fix dao

--- src/main/java/dev/m_pluse/com/dao/CustomerDao.java
+++ src/main/java/dev/m_pluse/com/dao/CustomerDao.java
@@ -4,7 +4,9 @@ package dev.m_pluse.com.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.m_pluse.com.entity.Customer;
-@Repository
+import org.springframework.stereotype.Repository;
+
+@Repository 
 public interface CustomerDao extends JpaRepository<Customer, Integer> {
 
 }
